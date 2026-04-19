10ea5e1b6a5f7541b166078ba8ddaf5698388f2d
seriousconsult <seriousconsult@hotmail.com>
2026-04-19 08:26:47 -0400

    add some starting files

---
+++ vpn/handshake.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+
+
+
+
+
+'''
+VPN servers often use specific versions of OpenSSL or BoringSSL. 
+A server can look at the JA3 hash of your SSL handshake.
+If that hash matches a known "NordVPN Exit Node" or "OpenVPN Client" signature,
+ they know you aren't just a regular person on Chrome.
+
+make script to detect
+
+
+NOT IMPLEMENTED
+'''
+
